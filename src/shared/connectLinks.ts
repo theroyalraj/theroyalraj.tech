f0addd1406610d2c105b3e2ded5cf855042daf2b
Utkarsh Raj <72919666+Utkarsh-raj-73@users.noreply.github.com>
2023-04-22 18:00:13 +0530

    style fix

--- src/shared/connectLinks.ts
+++ src/shared/connectLinks.ts
@@ -1,4 +1,4 @@
-const Mail = 'utraj00114@gmail.com';
+const Mail = 'raj.utkarsh001@gmail.com';
 const Gh = 'https://github.com/theroyalraj';
 const In = 'https://www.linkedin.com/in/theroyalraj/';
 
@@ -6,22 +6,22 @@ const connectLinks: {
   id: string;
   link: string;
 }[] = [
-  {
-    id: 'gmail',
-    link: 'mailto:utraj00114@gmail.com',
-  },
-  {
-    id: 'linkedin',
-    link: 'https://www.linkedin.com/in/theroyalraj/',
-  },
-  {
-    id: 'github',
-    link: 'https://github.com/theroyalraj',
-  },
-  {
-    id: 'whatsapp',
-    link: 'https://api.whatsapp.com/send/?phone=%2B917870455443&text=hi&type=phone_number&app_absent=0',
-  },
-];
+    {
+      id: 'gmail',
+      link: 'mailto:raj.utkarsh001@gmail.com',
+    },
+    {
+      id: 'linkedin',
+      link: 'https://www.linkedin.com/in/theroyalraj/',
+    },
+    {
+      id: 'github',
+      link: 'https://github.com/theroyalraj',
+    },
+    {
+      id: 'whatsapp',
+      link: 'https://api.whatsapp.com/send/?phone=%2B917870455443&text=hi&type=phone_number&app_absent=0',
+    },
+  ];
 
 export { Mail, Gh, In, connectLinks };
