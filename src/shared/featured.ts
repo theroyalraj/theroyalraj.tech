68972b0655fc041ce6ab5950ac8853c78299c844
Utkarsh Raj <72919666+Utkarsh-raj-73@users.noreply.github.com>
2023-05-02 08:36:15 +0530

    changes

--- src/shared/featured.ts
+++ src/shared/featured.ts
@@ -18,10 +18,11 @@ export const featured: {
     <ul type="disc">
   My role was to Design, develop, deploy, and maintain core services.
   <br><strong><u><center>Achievements/Tasks</center></u></strong>
-  <li>•Built core analysis engine V2 from the ground up using the Spring framework and Kubernetes architecture as microservices, resulting in a <strong>6x increase in load capacity.</strong></li>
-  <li>•Integrated <strong>ChatGPT</strong> with a live queuing server to provide personalized prompts, reducing onboarding time from one month to just a few hours.</li>
-  <li>•Created and scaled Webinar Bot using Kubernetes to facilitate attendance and recording for providers such as Zoom, MS Teams, and Google Meet. Leveraging puppeteer, Zoom SDK, and NodeJS in the process which can handle <strong>2k concurrent live webinars</strong>.</li>
+  <li>•Designed, structured, scaled, and developed core analysis engine V2 entirely from the ground up utilizing the spring framework, resulting in a <strong>6x increase in our load capacity</strong>.</li>
   <li>•Conducted Proof of Concept (PoC) and migrated reporting data from Postgres to Elasticsearch, resulting in a <strong>500% improvement in page load speed.</strong></li>
+  <li>•Integrated <strong>ChatGPT</strong> with a live queuing server to provide personalized prompts, reducing onboarding time from one month to just a few hours.</li>
+  <li>•Built and scaled Webinar Bot which facilitates attendance and recording for
+  providers such as Google Meet, MS Teams and Zoom. Leveraging puppeteer, Node.js and Kubernetes in the process which can handle <strong>2k concurrent live webinars</strong>.</li>
   <li>•Contributed to Proof of Concept (PoC) and aided in the development of a predictive model for forecasting won-loss opportunities using Google AutoML, which helped raise our series B funding.</li>
   <li>•Assisted with the development and productization of h20.ai AutoML model.</li>
   <li>•Utilized the Twilio SDK to establish Automated Voice Response systems that authenticate user phone numbers.</li>
@@ -39,7 +40,7 @@ export const featured: {
       name: 'Movie Recommendation System Using Netflix Dataset | Machine Learning',
       date: "(06/2019 - 07/2019)",
       info: "Made a Movie Recommendation based on user rating using Netflix Dataset.",
-      stack: ['Machine Leaning', 'SCIKIT-Learn', 'NumPy', 'Pandas', 'MatplotLib'],
+      stack: ['Machine Learning', 'SCIKIT-Learn', 'NumPy', 'Pandas', 'MatplotLib'],
       code: 'https://github.com/theroyalraj/Machine-Learning',
       live: 'https://github.com/theroyalraj/Machine-Learning',
       image: netflix,
